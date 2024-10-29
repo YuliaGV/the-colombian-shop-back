1c6bee4c9f7ec59518d0c2651b101c7dd557d415
YuliaGV <ygaviria7@gmail.com>
2024-10-28 22:06:13 -0500

    Initial commit

---
+++ src/main/java/com/yuliagv/the_colombian_shop_back/TheColombianShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.yuliagv.the_colombian_shop_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheColombianShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheColombianShopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuliagv/the_colombian_shop_back/TheColombianShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuliagv.the_colombian_shop_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheColombianShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
